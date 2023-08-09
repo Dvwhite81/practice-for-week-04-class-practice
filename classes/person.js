2bc337a61f7cebdc7d633de22dd07cfba4de512f
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-08-08 22:36:08 -0400

    Add files via upload

---
+++ classes/person.js
@@ -0,0 +1,34 @@
+// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(people) {
+    if(!Array.isArray(people)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (let person of people) {
+      if (person instanceof Person) {
+        person.introduce();
+      } else {
+        throw new Error("All items in array must be Person class instances.");
+      }
+    }
+  }
+}
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
